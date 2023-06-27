0a2066ca553e5b1997e0d119870d312a3a98ab4e
Valerie Dubach <valerieclaire96@gmail.com>
2023-06-27 10:21:51 -0400

    admin html and css

---
+++ admin/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+    </div>
+    </div>
+    <!-- Menu Section Ends -->
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            Main Content Goes Here
+        </div>
+    </div>
+    <!-- Main Content Section Ends -->
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2023 All Rights Reserved, Some Restaurant, Developed by <a href="https://github.com/Valerieclaire96">Valerie Dubach</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+
+</body>
+</html>
